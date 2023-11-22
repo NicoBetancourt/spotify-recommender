d8634a915a186b89caebd469232c0096b638939b
NicoBetancourt <jnbetancourtl@gmail.com>
2023-11-22 01:36:09 +0100

    Se agregan utilidades

--- app/utils/dateFormat.py
+++ app/utils/dateFormat.py
@@ -1,8 +1,15 @@
 
-import datetime
+from datetime import datetime
 
 class DateFormat():
     @classmethod
     def convert_data(self,date):
-        print(date)
-        return datetime.datetime.strftime(date,"%d/%m/%Y")
+        return datetime.strftime(date,"%d/%m/%Y")
+    
+    @classmethod
+    def str_to_date(self,date):
+        return datetime.strptime(date, "%d/%m/%Y")
+    
+    @classmethod
+    def date_to_str(self,date):
+        return datetime.strftime(date,"%d/%m/%Y")
